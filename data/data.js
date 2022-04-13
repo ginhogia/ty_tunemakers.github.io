ad051a3ba77585f65018e5f506f9c3429435f165
ginhojia <wainap@gmail.com>
2022-04-13 22:47:17 +0800

    Fist build

---
+++ data/data.js
@@ -0,0 +1,118 @@
+
+data={
+    "Questions":
+    [
+        {"QuestionNo": "c01", "Title":"關於<span class=\"questionTitleMarked\">乾燥</span>的肌膚問題",
+            "Answers":
+            {
+                "Line1":
+                [
+                {"AnswerNo":"01", "ProductNo":"O01", "Text":"因季節或身體狀況<br/>的變化而容易敏感"},
+                {"AnswerNo":"02", "ProductNo":"O01", "Text":"乾燥型<br/>敏感肌膚"},
+                {"AnswerNo":"03", "ProductNo":"O03", "Text":"乾燥到<br/>臉上起皮屑"},
+                {"AnswerNo":"04", "ProductNo":"O05", "Text":"肌膚摸起來<br/>粗糙、乾燥"}
+                ],
+                "Line2":
+                [
+                {"AnswerNo":"05", "ProductNo":"O02", "Text":"乾燥卻有粉刺"},
+                {"AnswerNo":"06", "ProductNo":"O02", "Text":"乾燥但T字部位泛油光"},
+                {"AnswerNo":"07", "ProductNo":"O04", "Text":"乾燥且暗沉"},
+                {"AnswerNo":"08", "ProductNo":"O09", "Text":"乾燥且毛孔粗大"}
+                ]
+            }
+        },
+        {"QuestionNo": "c02", "Title":"關於<span class=\"questionTitleMarked\">黑斑、暗沉</span>的肌膚問題",
+        "Answers":
+        {
+            "Line1":
+            [
+            {"AnswerNo":"01", "ProductNo":"O13", "Text":"黑斑、雀斑、暗沉"},
+            {"AnswerNo":"02", "ProductNo":"O10", "Text":"因日晒而產生黑斑"},
+            {"AnswerNo":"03", "ProductNo":"O12", "Text":"容易有黑斑、暗沉"}
+            ],
+            "Line2":
+            [
+            {"AnswerNo":"04", "ProductNo":"O12", "Text":"肌膚整體暗沉<br/>且粗糙"},
+            {"AnswerNo":"05", "ProductNo":"O13", "Text":"肌膚整體暗沉<br/>且毛孔粗大"}
+            ]
+        }
+    
+        },
+        {"QuestionNo": "c03", "Title":"關於<span class=\"questionTitleMarked\">抗老修護</span>的肌膚問題",
+        "Answers":
+        {
+            "Line1":
+            [
+            {"AnswerNo":"01", "ProductNo":"O07", "Text":"眼角皺紋"},
+            {"AnswerNo":"02", "ProductNo":"O04", "Text":"眉間、額頭的皺紋"},
+            {"AnswerNo":"03", "ProductNo":"O07", "Text":"法令紋"},
+            {"AnswerNo":"04", "ProductNo":"O09", "Text":"臉部缺乏緊緻且鬆弛"},
+            {"AnswerNo":"05", "ProductNo":"O11", "Text":"毛孔鬆弛"}
+            ],
+            "Line2":
+            [
+            {"AnswerNo":"06", "ProductNo":"O10", "Text":"鬆弛且有黑斑"},
+            {"AnswerNo":"07", "ProductNo":"O08", "Text":"缺乏緊緻、肌膚粗糙"},
+            {"AnswerNo":"08", "ProductNo":"O08", "Text":"毫無緊緻與光澤"},
+            {"AnswerNo":"09", "ProductNo":"O09", "Text":"因年齡造成的乾燥"}
+            ]
+        }
+    },
+        {"QuestionNo": "c04", "Title":"關於<span class=\"questionTitleMarked\">毛孔</span>的肌膚問題",
+        "Answers":
+        {
+            "Line1":
+            [
+            {"AnswerNo":"01", "ProductNo":"N01", "Text":"有黑頭粉刺"},
+            {"AnswerNo":"02", "ProductNo":"O09", "Text":"毛孔粗大"},
+            {"AnswerNo":"03", "ProductNo":"O11", "Text":"毛孔鬆弛"},
+            {"AnswerNo":"04", "ProductNo":"O06", "Text":"毛孔粗大、肌膚泛油光"}
+            ],
+            "Line2":
+            [
+            {"AnswerNo":"05", "ProductNo":"N01", "Text":"毛孔粗大<br/>、肌膚整體暗沉"},
+            {"AnswerNo":"06", "ProductNo":"O06", "Text":"毛孔粗大<br/>、暗沉且肌膚乾燥"},
+            {"AnswerNo":"07", "ProductNo":"N02", "Text":"有黑頭粉刺<br/>且鼻頭乾燥"}
+            ]
+        }
+    },
+        {"QuestionNo": "c05", "Title":"關於<span class=\"questionTitleMarked\">肌膚粗糙</span>的肌膚問題",
+        "Answers":
+        {
+            "Line1":
+            [
+            {"AnswerNo":"01", "ProductNo":"O06", "Text":"粉刺"},
+            {"AnswerNo":"02", "ProductNo":"O09", "Text":"長粉刺且乾燥"},
+            {"AnswerNo":"03", "ProductNo":"O06", "Text":"T字部位長粉刺"},
+            {"AnswerNo":"04", "ProductNo":"O06", "Text":"U字部位長粉刺"}
+            ],
+            "Line2":
+            [
+            {"AnswerNo":"05", "ProductNo":"O05", "Text":"肌膚粗糙<br/>、乾燥"},
+            {"AnswerNo":"06", "ProductNo":"O10", "Text":"肌膚粗糙<br/>、黑斑"},
+            {"AnswerNo":"07", "ProductNo":"O08", "Text":"肌膚粗糙<br/>且肌膚整體暗沉"},
+            {"AnswerNo":"08", "ProductNo":"O08", "Text":"肌膚粗糙<br/>且臉部整體鬆弛"}
+            ]
+        }
+    }
+    ],
+    
+    "Products":
+    [
+        {"ProductNo":"O01", "No":"1", "Name":"神經醯胺200前導原液", "Title2":"強化修護肌膚屏障,<br/>幫助後續保養", "Price":"NT1,750元", "Volume":"60ml", "Pic":"pic/O01_hero.png", "PicResult":"pic/result_1.png", "PicDetail":"pic/detail_1.png", "Text":"神經醯胺為肌膚的重要原生組成，有效補充能幫助填補肌膚鎖水層、鞏固肌膚屏障，並增加肌膚防禦力，開啟後續保養吸收通道。"},
+        {"ProductNo":"O02", "No":"2", "Name":"滲透型玻尿酸保濕原液", "Title2":"快速補水,維持肌膚彈力", "Price":"NT700元", "Volume":"20ml", "Pic":"pic/O02_hero.png", "PicResult":"pic/result_2.png", "PicDetail":"pic/detail_2.png", "Text":"高濃度小分子玻尿酸，更好吸收，能滲入肌膚底層、密集補水，減緩因肌膚乾燥而產生的肌膚瑕疵及粗糙。"},
+        {"ProductNo":"O03", "No":"3", "Name":"角鯊烷保濕修護原液", "Title2":"純化角鯊烷精華油,<br/>潤澤修護肌膚", "Price":"NT680元", "Volume":"20ml", "Pic":"pic/O03_hero.png", "PicResult":"pic/result_3.png", "PicDetail":"pic/detail_3.png", "Text":"高純度明星級修護成分角鯊烷保養油，修護肌膚皮脂膜，打造健康潤澤機，能加入其他保養品增加滋潤、按摩修護全臉。"},
+        {"ProductNo":"O04", "No":"4", "Name":"日本酒發酵保溼原液", "Title2":"高效保濕,撫平乾燥細紋", "Price":"NT880元", "Volume":"10ml", "Pic":"pic/O04_hero.png", "PicResult":"pic/result_4.png", "PicDetail":"pic/detail_4.png", "Text":"日本純米發酵，高光酵醒熬夜肌。利用微生物發酵，釋放出微量的糖和氨基酸，加上多種美容成分，提高肌膚的保濕抗氧化修復能力。"},
+        {"ProductNo":"O05", "No":"5", "Name":"蘆薈保濕調理原液", "Title2":"保濕修護,舒緩肌膚不適", "Price":"NT480元", "Volume":"10ml", "Pic":"pic/O05_hero.png", "PicResult":"pic/result_5.png", "PicDetail":"pic/detail_5.png", "Text":"由新鮮蘆薈萃取，本身具有良好的保水功能，可為肌膚帶來大量水分，更可安撫肌膚不適，舒緩曬後損傷"},
+        {"ProductNo":"O06", "No":"6", "Name":"甘草草本舒緩調理原液", "Title2":"舒緩肌膚,修護肌膚屏障", "Price":"NT480元", "Volume":"10ml", "Pic":"pic/O06_hero.png", "PicResult":"pic/result_6.png", "PicDetail":"pic/detail_6.png", "Text":"高濃度甘草精粹，全臉使用能協助油水平衡、調理毛孔，並穩定膚。無油配方，痘肌也可以塗抹濕敷，幫助舒緩救急。"},
+        {"ProductNo":"O07", "No":"7", "Name":"維他命A醇賦活原液", "Title2":"淡化眼周乾燥細紋,<br/>有效修護肌膚", "Price":"NT650元", "Volume":"10ml", "Pic":"pic/O07_hero.png", "PicResult":"pic/result_7.png", "PicDetail":"pic/detail_7.png", "Text":"明星級抗老成分–A醇衍生物，每日使用幫助預防細紋產生，緊緻並賦活肌膚，抵禦肌膚老化徵兆。"},
+        {"ProductNo":"O08", "No":"8", "Name":"維生素E原液", "Title2":"抗氧化維生素E衍生物添加,<br/>有效修護肌膚", "Price":"NT480元", "Volume":"10ml", "Pic":"pic/O08_hero.png", "PicResult":"pic/result_8.png", "PicDetail":"pic/detail_8.png", "Text":"高濃度維生素E，從肌膚底層守住水潤，延緩老化，維持肌膚青春。"},
+        {"ProductNo":"O09", "No":"9", "Name":"滲透型膠原蛋白原液", "Title2":"幫助維持肌膚彈力,<br/>使肌膚澎潤彈嫩", "Price":"NT880元", "Volume":"20ml", "Pic":"pic/O09_hero.png", "PicResult":"pic/result_9.png", "PicDetail":"pic/detail_9.png", "Text":"複合大小分子高濃度膠原蛋白成分，小分子膠原蛋白易吸收，大分子膠原蛋白能幫助肌膚提升保水力，維持肌膚彈潤飽滿緊實。"},
+        {"ProductNo":"O10", "No":"10", "Name":"富勒烯全面修護原液", "Title2":"高效抗氧化成分,<br/>幫助防止肌膚老化,全面修護肌膚", "Price":"NT1,170元", "Volume":"10ml", "Pic":"pic/O10_hero.png", "PicResult":"pic/result_10.png", "PicDetail":"pic/detail_10.png", "Text":"高濃度富勒烯–超強抗老成分，能從根源全面抵禦肌膚老化。幫助防止細紋、暗沉等老化瑕疵形成。"},
+        {"ProductNo":"O11", "No":"11", "Name":"EGF煥顏修護原液", "Title2":"幫助延緩肌膚老化,<br/>淡化肌膚乾燥細紋", "Price":"NT1,170元", "Volume":"10ml", "Pic":"pic/O11_hero.png", "PicResult":"pic/result_11.png", "PicDetail":"pic/detail_11.png", "Text":"EFG(Epidermal Growth Factor)能從肌底活化肌膚，幫助調理肌膚紋路及撫平乾燥細紋，防止肌膚老化。"},
+        {"ProductNo":"O12", "No":"12", "Name":"菸鹼醯胺亮白原液", "Title2":"改善肌膚暗沉,<br/>強化肌膚保護力,亮白肌膚", "Price":"NT480元", "Volume":"10ml", "Pic":"pic/O12_hero.png", "PicResult":"pic/result_12.png", "PicDetail":"pic/detail_12.png", "Text":"菸鹼醯胺亮白成分，溫和好吸收，改善膚色不均，膚色散發亮白光采。"},
+        {"ProductNo":"O13", "No":"13", "Name":"傳明酸淨白原液", "Title2":"從源頭高校淨白肌膚", "Price":"NT1,750元", "Volume":"10ml", "Pic":"pic/O13_hero.png", "PicResult":"pic/result_13.png", "PicDetail":"pic/detail_13.png", "Text":"高濃度明星級亮白成分傳明酸，從源頭明亮肌底，掃除瑕疵，深度淨白。"},
+        {"ProductNo":"N01", "No":"", "Name":"AHA角質對策水", "Title2":"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;", "Price":"NT580元", "Volume":"120ml", "Pic":"pic/N01_hero.png", "PicResult":"pic/result_14.png", "PicDetail":"pic/detail_14.png", "Text":"AHA果酸原液添加，促進代謝角質，細緻毛孔。"},
+        {"ProductNo":"N02", "No":"", "Name":"甘草草本舒緩調理水", "Title2":"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;", "Price":"NT780元", "Volume":"120ml", "Pic":"pic/N02_hero.png", "PicResult":"pic/result_15.png", "PicDetail":"pic/detail_15.png", "Text":"添加甘草根萃取原液，協助油水平衡，舒緩安撫，維持肌膚健康狀態。"},
+    ]
+}
